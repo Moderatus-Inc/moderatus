25c6b1309a316f44b5f7de8abc2f8796c3c1b97f
MrMudkip <60580452+MrMudkip@users.noreply.github.com>
2020-09-01 20:26:00 +0100

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,33 @@
+import discord
+from discord.ext import commands
+from pretty_help import PrettyHelp
+
+def get_prefix(client, message):
+
+    prefixes = ['m!', 'm1']
+
+    if not message.guild:
+        prefixes = ['m!']
+
+    return commands.when_mentioned_or(*prefixes)(client, message)
+
+bot = commands.Bot(                         
+    command_prefix=get_prefix,              
+    help_command=PrettyHelp(color=discord.Color.dark_red()),
+    description='A bot used for moderation. I can do ban and kick punishments. I cannot, however unban right now.',
+    owner_id=541872670371741697,            
+    case_insensitive=False,
+    allowed_mentions=discord.AllowedMentions(roles=False, everyone=False)
+)
+
+@bot.event
+async def on_ready():                                       
+    print(f'Logged in as {bot.user.name} - {bot.user.id}')
+    await bot.change_presence(status=discord.Status.dnd, activity=discord.Activity(type=discord.ActivityType.watching, name=f'{len(bot.users)} user(s) in {len(bot.guilds)}/100 server(s) | m!help or m1help'))
+    bot.load_extension("cogs.moderation")
+    bot.load_extension("cogs.misc")
+    bot.load_extension("cogs.music")
+    bot.load_extension("cogs.owner")
+    return
+    
+bot.run('TOKEN', bot=True, reconnect=True)
