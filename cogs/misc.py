cdaee87c61c462fa717d9604810bdc0b8a539175
Leon Peter Keates <60580452+MrDragonBoi@users.noreply.github.com>
2020-11-15 12:02:59 +0000

    Update misc.py

--- cogs/misc.py
+++ cogs/misc.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 import traceback
 import sys
+import os
+import inspect
 
 class Misc(commands.Cog):
     """Hmmm, what could these be for?"""
@@ -18,7 +20,7 @@ class Misc(commands.Cog):
         embed.add_field(name="Developer:", value="`MrDragonBoi ඞ#7894`", inline=True)
         embed.add_field(name="Library:", value="`discord.py`", inline=True)
         embed.add_field(name="Servers:", value=f"`{len(self.bot.guilds)}`", inline=True)
-        embed.add_field(name="Version:", value="`Alpha 1.5`", inline=True)
+        embed.add_field(name="Version:", value="`Alpha 1.7`", inline=True)
         embed.add_field(name="Support:", value=":star: [Here](https://discord.gg/EyKqRNT)", inline=True)
         embed.add_field(name="Invite:", value=":robot: [Invite](https://discord.com/oauth2/authorize?client_id=734822514894831639&permissions=2134240759&scope=bot)", inline=True)
         embed.add_field(name="Vote:", value=":white_check_mark: [Vote](https://discord.ly/moderatus/upvote)", inline=True)
@@ -31,7 +33,7 @@ class Misc(commands.Cog):
     @commands.command(name="ping")
     async def ping(self, ctx):
         """Shows the bot's ping"""
-        embed=discord.Embed(title="Latency", description=f'{round(ctx.bot.latency * 1000)}ms')
+        embed=discord.Embed(title="PONG!", description=f'My latency is at least 69% likely to be: **{round(ctx.bot.latency * 1000)}ms**')
         await ctx.send(embed=embed)
 
     @commands.command(name="serverinfo", aliases=['si'])
@@ -66,39 +68,74 @@ class Misc(commands.Cog):
 
     @commands.command(name="bugreport")
     @commands.cooldown(1, 10, commands.BucketType.user)
-    async def bug(self, ctx):
+    async def bug(self, ctx, message):
         """Report bugs directly to the owner!"""
         channel = self.bot.get_channel(762306208580370464)
         embed = discord.Embed(title='Bug report!', description='Your bug report, along with your discord name, discriminator, and id were sent to the bot owner. He will look into your report and hopefully fix the issue soon!', colour=discord.Color.green())
-        if channel is not None:
+        if channel is None:
+            await ctx.send('Unable to send request.')
+        else:
             await channel.send(f'{ctx.message.content} | User: {ctx.message.author} ID: {ctx.message.author.id}')
             await ctx.send(embed=embed)
-        else:
-            await ctx.send('Unable to send request.')
 
     @commands.command(name="suggest")
     @commands.cooldown(1, 10, commands.BucketType.user)
-    async def suggest(self, ctx):
+    async def suggest(self, ctx, message):
         """Suggest features directly to the owner! Make sure you have DMs on and join the server!"""
         channel = self.bot.get_channel(762306208580370464)
         embed = discord.Embed(title='Suggestion!', description='Your suggestion, along with your discord name, discriminator, and id were sent to the bot owner. He will look into your report and hopefully add the feature soon!', colour=discord.Color.green())
-        if channel is not None:
+        if channel is None:
+            await ctx.send('Unable to send request.')
+        else:
             await channel.send(f'{ctx.message.content} | User: {ctx.message.author} ID: {ctx.message.author.id}')
             await ctx.send(embed=embed)
-        else:
-            await ctx.send('Unable to send request.')
 
     @commands.command(name="news")
     @commands.cooldown(1, 10, commands.BucketType.user)
     async def news(self, ctx):
         """Announcements regarding Moderatus. Make sure to join the server as well!"""
         embed = discord.Embed(title='What\'s New?', description='Here is the new stuff added/removed!')
-        embed.add_field(name="Current version:", value="Alpha 1.5")
-        embed.add_field(name="Added", value="N/A")
+        embed.add_field(name="Current version:", value="Alpha 1.7")
+        embed.add_field(name="Added", value="Prefix command (about time!) + source command (links to GitHub)")
         embed.add_field(name="Removed", value="N/A")
-        embed.add_field(name="Other", value="Profile pic changed for server & bot (Premium will change asap)")
-        embed.add_field(name="Fixed", value="Music commands <:sharkyay:744345667882975232>")
+        embed.add_field(name="Other", value="This do be quick tho")
+        embed.add_field(name="Fixed", value="N/A")
         await ctx.send(embed=embed)
+
+    @commands.command()
+    async def source(self, ctx, *, command: str = None):
+        """Displays my full source code or for a specific command."""
+        source_url = 'https://github.com/moderatus-inc/moderatus'
+        branch = 'master'
+        if command is None:
+            return await ctx.send(source_url)
+
+        if command == 'help':
+            src = type(self.bot.help_command)
+            module = src.__module__
+            filename = inspect.getsourcefile(src)
+        else:
+            obj = self.bot.get_command(command.replace('.', ' '))
+            if obj is None:
+                return await ctx.send('Could not find command.')
+
+            # since we found the command we're looking for, presumably anyway, let's
+            # try to access the code itself
+            src = obj.callback.__code__
+            module = obj.callback.__module__
+            filename = src.co_filename
+
+        lines, firstlineno = inspect.getsourcelines(src)
+        if not module.startswith('discord'):
+            # not a built-in command
+            location = os.path.relpath(filename).replace('\\', '/')
+        else:
+            location = module.replace('.', '/') + '.py'
+            source_url = 'https://github.com/moderatus-inc/moderatus'
+            branch = 'master'
+
+        final_url = f'<{source_url}/blob/{branch}/{location}#L{firstlineno}-L{firstlineno + len(lines) - 1}>'
+        await ctx.send(final_url)
 
 def setup(bot):
     bot.add_cog(Misc(bot))
