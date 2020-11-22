852946ecfa4d49ab7b409a2a1e8c5eccd4bed5ea
Leon Peter Keates <60580452+MrDragonBoi@users.noreply.github.com>
2020-11-22 15:43:42 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,13 @@ from discord.ext import commands
 import asyncio
 from pretty_help import PrettyHelp
 import json
+from pathlib import Path
+from discord import Colour
+import sys
+
+cwd = Path(__file__).parents[0]
+cwd = str(cwd)
+print(f"{cwd}\n-----")
 
 def prefix(bot, message):
     with open("prefixes.json") as f:
@@ -54,14 +61,7 @@ async def snipe(ctx, *, channel: discord.TextChannel = None):
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user.name} - {bot.user.id}')
-    await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name=f'{len(bot.guilds)} server(s) | m!help or m1help | moderatus.xyz'))
-    bot.load_extension("cogs.moderation")
-    bot.load_extension("cogs.misc")
-    bot.load_extension("cogs.music")
-    bot.load_extension("cogs.owner")
-    bot.load_extension("cogs.fun")
     bot.loop.create_task(servers())
-    return
 
 @bot.event
 async def on_guild_join(guild):
@@ -78,7 +78,7 @@ async def on_guild_remove(guild):
     with open('prefixes.json', 'r') as f:
         prefixes = json.load(f)
     
-    prefixes.pop[str(guild.id)]
+    prefixes.pop(str(guild.id))
 
     with open('prefixes.json', 'w') as f:
         json.dump(prefixes, f, indent=4)
@@ -99,14 +99,34 @@ async def _prefix(ctx, prefix):
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.NotOwner):
-        await ctx.send(f'Nice try! You can\'t use owner commands if you\'re not MrDragonBoi ඞ#7894. He is the only one with the perms.')
+        embed = discord.Embed(title="Frick!", description="You tried to use developer commands, but guess what? You're not MrDragonBoi ඞ#7894! Also, don't even think about hacking his account, you idot")
+        await ctx.send(embed=embed)
     if isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send(f'You didn\'t specify the right arguments for this command.')
-    if isinstance(error, commands.BotMissingPermissions):
-        await ctx.send(f'I don\'t have the perms to perform this action.')
+        embed = discord.Embed(title="Frick!", description="You never specified the right arguments. Like, go get a life, n00b!")
+        await ctx.send(embed=embed)
+    if isinstance(error, commands.CommandOnCooldown):
+        embed = discord.Embed(title="Frick!", description="This command is on cooldown. Probably since you're going too fast lol n00b")
+        await ctx.send(embed=embed)
     if isinstance(error, commands.MissingPermissions):
-        await ctx.send(f'You don\'t have the perms to perform this action.')
+        embed = discord.Embed(title="Frick!", description="I don't have the perms for this command, maybe see about giving me the right perms?")
+        await ctx.send()
     if isinstance(error, commands.NSFWChannelRequired):
-        await ctx.send(f'This command does not work in non-NSFW channels. Apologies')
+        embed = discord.Embed(title="Frick!", description="This command is ONLY available in NSFW channels. Nice try, n00b!")
+        await ctx.send(embed=embed)
 
-bot.run('Super duper secret token no looky 👀', bot=True)
+def read_json(filename):
+    with open(f"{cwd}/{filename}.json", "r") as file:
+        data = json.load(file)
+    return data
+
+def write_json(data, filename):
+    with open(f"{cwd}/{filename}.json", "w") as file:
+        json.dump(data, file, indent=4)
+
+bot.load_extension("cogs.moderation")    
+bot.load_extension("cogs.misc")
+bot.load_extension("cogs.music")
+bot.load_extension("cogs.owner")
+bot.load_extension("cogs.fun")
+bot.load_extension("jishaku")
+bot.run('TOKEN', bot=True)
